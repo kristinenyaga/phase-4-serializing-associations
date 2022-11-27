1bf3a33bafeaa95fe8a39270705cb441ab4741e8
krisine <kristine.nyaga@student.moringaschool.com>
2022-11-27 08:03:08 +0300

    done

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
-  
+  has_many :reviews
+  belongs_to :director
 end
